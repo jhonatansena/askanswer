0b473994143e8824f2c71966bf232e4c7ed65c48
Jhonatan Sena <jcostasenna@hotmail.com>
2021-06-08 19:42:37 -0300

    Criado arquivos de ambientes para proteger senhas de acessos

--- database/database.js
+++ database/database.js
@@ -1,7 +1,9 @@
-// const { Sequelize } = require('sequelize');
 const Sequelize = require('sequelize'); 
+require('dotenv/config');
 
-const connection = new Sequelize('askanswe', 'root', 'root',{
+
+const connection = new Sequelize(process.env.DATA_BASE,
+     process.env.USERNAME, process.env.PASSWORD,{
     host: 'localhost',
     dialect: 'mysql',
     logging: false
